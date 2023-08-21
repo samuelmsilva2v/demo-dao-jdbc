60a3621a236c11dedf097a6d50650a26373dc2bf
Samuel Maciel <samuelmsilva@outlook.com.br>
2023-08-21 15:55:12 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,9 +9,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
+		Department dep = new Department(1, "Books");
 		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dep);
 		
 		System.out.println(seller);
 
